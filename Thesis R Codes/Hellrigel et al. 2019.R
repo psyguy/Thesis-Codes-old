715ca5d3edf760ff4ba93ce215d1ec8d9501dc8d
Manuel Haqi <mhscientist@gmail.com>
2019-02-24 19:43:39 +0100

    The clustering coefficient doesn't change over time.

--- Thesis R Codes/Hellrigel et al. 2019.R
+++ Thesis R Codes/Hellrigel et al. 2019.R
@@ -8,7 +8,7 @@ num.nodes <- 300
 eps <- c(0.3, 0.4, 0.5)
 a <- 1.7
 t.transient <- 20 %>% as.vector()
-num.epochs <- 50000
+num.epochs <- 10000
 tol <- 0.001
 
 conn <- make.random.graph(size = num.nodes, num.links = num.edges,
@@ -28,7 +28,7 @@ for(iterations in 1:num.epochs){
   
   if(!(iterations %% 100)) paste("System time at iteration", iterations, "is", Sys.time()) %>% print()
 
-  rewired <- my.rewiring(inp.x = x, inp.conn = connectivity.matrix, eps = eps[1])
+  rewired <- my.rewiring(inp.x = x, inp.conn = connectivity.matrix, eps = eps[3])
   x <- rewired$x
   connectivity.matrix <- rewired$connectivity.matrix  
   clustering.coef[iterations] <- rewired$clustering.coef
@@ -38,3 +38,6 @@ time.end <- Sys.time()
 time.taken <- time.end - time.start
 paste("It took", time.taken, "to run for", num.epochs, "epochs.") %>% print()
 
+current.status <- list(rewired = rewired, clustering.coef = clustering.coef, num.epochs = num.epochs, eps = eps[3], seed = 1)
+
+save(current.status, file = "current.status.20180224.2.RData")
